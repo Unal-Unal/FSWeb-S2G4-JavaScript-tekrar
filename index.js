c60804b33b72ceeda7d5e57c6cc3c83ca6bf1e8d
Unal-Unal <unal.unal.cv@gmail.com>
2025-03-22 18:08:21 +0300

    solution of index js

--- index.js
+++ index.js
@@ -50,8 +50,8 @@ function KareninAlani(kenaruzunlugu) {
 	4. Hesaplanan çemberin çevresi döndürülecektir.
 */
 
-function CemberinCevresi(/* kodlar buraya */) {
-  /* kodlar buraya */
+function CemberinCevresi(yaricap) {
+  return 2 * pi * yaricap;
 }
 
 /* (Oto test yok) Yukarıdaki CemberinCevresi fonksiyonunu yarıçap = 5 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
@@ -64,8 +64,8 @@ function CemberinCevresi(/* kodlar buraya */) {
 	4. Hesaplanan çemberin alanı döndürülecektir.
 */
 
-function CemberinAlani(/* kodlar buraya */) {
-  /* kodlar buraya */
+function CemberinAlani(yaricap, pi) {
+  return pi * yaricap * yaricap;
 }
 
 /* (Oto test yok) Yukarıdaki CemberinAlani fonksiyonunu yarıçap = 15 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
@@ -98,28 +98,60 @@ let ucetambolunenler,
   tekraredensayilar;
 
 // 3a çözümü
+enkucuk = sayilar[0];
+enbuyuk = sayilar[0];
 
-/* kodlar buraya */
+for (let i = 0; i < sayilar.length; i++) {
+  const sayi = sayilar[i];
+
+  if (sayi > enbuyuk) {
+    enbuyuk = sayi;
+  }
+
+  if (sayi < enkucuk) {
+    enkucuk = sayi;
+  }
+}
 
 // 3b çözümü:
 
-/* kodlar buraya */
+  ucetambolunenler = [];
+  sayilar.forEach((sayi) => {
+    if(sayi %3 === 0) {
+      ucetambolunenler.push(sayi);
+    }
+  });
 
 // 3c çözümü:
 
-/* kodlar buraya */
+ucebolunenlerintoplami = ucetambolunenler.reduce((toplam, sayi) => toplam + sayi, 0);
 
 // 3d çözümü
 
-/* kodlar buraya */
+besyuzdenkucuksayilar = sayilar.filter((sayi) => sayi < 500);
 
 // 3e çözümü
 
-/* kodlar buraya */
+siralisayilar = besyuzdenkucuksayilar.sort((a, b) => a - b);
 
 // 3f çözümü
 
-/* kodlar buraya */
+let tekraredensayilarObje = {};
+sayilar.forEach(sayi => {
+  if(!tekraredensayilarObje[sayi]) {
+    tekraredensayilarObje[sayi] = 1; 
+  } else {
+    tekraredensayilarObje[sayi]++;
+  }
+})
+ tekraredensayilar = [];
+
+for (let key in tekraredensayilarObje) {
+const value = tekraredensayilarObje[key];
+if (value > 1 ) {
+  tekraredensayilar.push(`${key} sayısı ${value} kere tekrar edilmiştir.`)
+}
+}
 
 /*  Bu satırın aşağısındaki kodları lütfen değiştirmeyin  */
 
